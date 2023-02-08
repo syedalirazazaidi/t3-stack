8b82b3f501e3955a564da19a2490f2863c3f6ed3
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-02-09 00:42:48 +0500

    add new challanges

--- reactchallanges/challesges/src/App.tsx
+++ reactchallanges/challesges/src/App.tsx
@@ -1,57 +1,63 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { background, Button } from "@chakra-ui/react";
-import { Box, Text } from "@chakra-ui/layout";
+import { Box, Stack, Text } from "@chakra-ui/layout";
 
 function App() {
-  const [colour, setClour] = useState("#808080");
-
+  const [colour, setClour] = useState<string>("");
+  const [answer, setAnswer] = useState<string[]>([]);
+  const [check, setanswer] = useState("");
+  function get_random_color() {
+    function c() {
+      var hex = Math.floor(Math.random() * 256).toString(16);
+      return ("0" + String(hex)).substr(-2); // pad with zero
+    }
+    return "#" + c() + c() + c();
+  }
+  useEffect(() => {
+    const newcolor = get_random_color();
+    setClour(newcolor);
+    setAnswer([newcolor, get_random_color(), get_random_color()].sort());
+  }, []);
+  console.log(answer, "ANSWER");
+  const checkAnswer = (anss: string) => {
+    if (colour === anss) {
+      setanswer("Correct answer");
+    } else {
+      setanswer("wrong answer");
+    }
+  };
   return (
-    // #808080
-    // #800080
-    // #FF0000
-    // #0000FF
     <div className="App">
-      <Box
-        bg={colour}
-        width={"300px"}
-        height={"300px"}
-        onClick={() => setClour("#800080")}
-      ></Box>
-      <Box
-        display={"flex"}
-        gap="4px"
-        p="10px"
-        alignItems={"center"}
-        textAlign={"center"}
-        marginRight="10px"
-      >
-        <Button
-          onClick={() => setClour("#800080")}
-          _hover={{
-            cursor: "pointer",
-          }}
+      <Stack>
+        <Box
+          bg={colour}
+          width={"300px"}
+          height={"300px"}
+          onClick={() => setClour(colour)}
+        ></Box>
+        <Box> {check} </Box>
+        <Box
+          display={"flex"}
+          gap="4px"
+          p="10px"
+          alignItems={"center"}
+          textAlign={"center"}
+          marginRight="10px"
         >
-          #800080
-        </Button>
-        <Button
-          onClick={() => setClour("#FF0000")}
-          _hover={{
-            cursor: "pointer",
-          }}
-        >
-          #FF0000
-        </Button>
-        <Button
-          onClick={() => setClour("#0000FF")}
-          _hover={{
-            cursor: "pointer",
-          }}
-        >
-          #0000FF
-        </Button>
-      </Box>
+          {answer.map((ans) => (
+            <Button
+              onClick={() => checkAnswer(ans)}
+              _hover={{
+                cursor: "pointer",
+              }}
+            >
+              {ans}
+            </Button>
+          ))}
+        </Box>
+      </Stack>
     </div>
   );
 }
