af88eb5ddfd3c847e201d6808f1ba4cd3dde7b66
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-02-11 23:11:50 +0500

    add new pro

---
+++ structure/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [arr, setArr] = useState(["a", "b", "c"]);
+
+  return <div className="App">
+    
+  </div>;
+}
+
+export default App;
